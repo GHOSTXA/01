db1ab306df12bfd6c519af166e62a58f3256946a
GHOSTXA <113169212+GHOSTXA@users.noreply.github.com>
2022-09-16 01:58:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 let fs = require('fs')                    //CREATOR = GEMPY , DEVBOT
 let chalk = require('chalk')
 global.owner = [
-  ['6287898307350'],
-  ['66287898307350'],
-  ['6287898307350', 'gempy', true]  
+  ['6288279174046'],
+  ['6288279174046'],
+  ['6288279174046', 'gempy', true]  
   
 ] 
-global.ownername = [['Gempy']]
+global.ownername = [['Hanz']]
 global.namabot = [['PENTABOT']]
 global.ownerinsta = [['@gmpyyy']]
 global.mods = [] 
@@ -43,8 +43,8 @@ process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0
 const spack = fs.readFileSync("lib/exif.json")
 const stickerpack = JSON.parse(spack)
 if (stickerpack.spackname == '') {
-  var sticker_name = 'PENTABOT'
-  var sticker_author = 'By GEMPY'
+  var sticker_name = 'HANZ'
+  var sticker_author = 'By FARHAN'
 } else {
   var sticker_name = stickerpack.spackname
   var sticker_author = stickerpack.sauthor
